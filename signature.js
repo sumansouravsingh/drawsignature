37e28a3ebaf4f0cbb1c1779a5ce89d1688bd6d55
sumansouravsingh <sumansouravsingh999@gmail.com>
2016-11-16 02:13:52 -0500

    Bug Fix

--- signature.js
+++ signature.js
@@ -1,19 +1,30 @@
 var cx=0,height,width;
 var mouseX=0,mouseY=0,mouseXinit=0,mouseYinit=0;
 var reg= new RegExp("^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$");
-function drawSignature(w,h,clr){
+
+function drawSignature(w,h,clr,isBorder){
     var mousedown=false;
     height=h;width=w;
     if(!reg.test(clr))
         clr="#000000";
     var cElem = document.createElement("canvas");
+    setCanvasProperties(cElem,h,w,isBorder);
+    var ctx = cElem.getContext("2d");
+    ctx.fillStyle="#ffffff";
+    handleMouseFunctions(cElem,ctx,mousedown,clr);
+}
+
+function setCanvasProperties(cElem,h,w,isBorder)
+{
     cElem.setAttribute("id","signature-canvas");
     cElem.setAttribute("height",h);
     cElem.setAttribute("width",w);
-    cElem.setAttribute("style","border :1px solid black");
+    if(isBorder)cElem.setAttribute("style","border :1px solid black");
     document.getElementById('mycanvas').appendChild(cElem);
-    var ctx = cElem.getContext("2d");
-    ctx.fillStyle="#ffffff";
+}
+
+function handleMouseFunctions(cElem,ctx,mousedown,clr)
+{
     cElem.addEventListener( 'mousemove', function( event ) {
         mouseX = event.offsetX;
         mouseY = event.offsetY;
@@ -21,14 +32,15 @@ function drawSignature(w,h,clr){
         {
             mouseXinit=mouseX;
             mouseYinit=mouseY;
+            console.log
             cx++;
         }
         sign(ctx,mousedown,clr);
     });
     cElem.addEventListener( 'mouseout', function( event ) {
         mousedown=false;
+        cx=0;
     });
-
     cElem.addEventListener( 'mousedown', function( event ) {
         mousedown = true;
     });
@@ -39,7 +51,7 @@ function drawSignature(w,h,clr){
 }
 
 function sign(ctx,mousedown,clr) {
-  ctx.strokeStyle = clr; 
+  ctx.strokeStyle = clr;
   if (mousedown) {
     ctx.beginPath();
     ctx.moveTo(mouseXinit,mouseYinit);
